8b5c24f2c051f149df047a53255506723d05e7d5
investimento23 <investimentosa867@gmail.com>
2025-05-04 01:31:59 +0200

    Update enviarSMS.js

--- enviarSMS.js
+++ enviarSMS.js
@@ -12,18 +12,29 @@ async function enviarSMS(to, message) {
     message
   });
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "apiKey": apiKey,
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: params
-  });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        apiKey,
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: params
+    });
 
-  const data = await response.json();
-  console.log(data);
-  return data;
+    const data = await response.json();
+
+    if (data.SMSMessageData && data.SMSMessageData.Message === "Sent to 1/1 Total Cost:") {
+      console.log("SMS enviado com sucesso.");
+    } else {
+      console.log("Resposta da API:", data);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Erro ao enviar SMS:", error);
+    return null;
+  }
 }
 
 module.exports = enviarSMS;

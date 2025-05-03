25e075c75f36a61d7230a10791b20d12857a6f0f
investimento23 <investimentosa867@gmail.com>
2025-05-04 01:33:56 +0200

    Update Investimento.js

--- models/Investimento.js
+++ models/Investimento.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-const InvestimentoSchema = new mongoose.Schema({
-  telefone: String,
-  valor: Number,
-  referencia: String,
-  criadoEm: {
-    type: Date,
-    default: Date.now
-  }
+const investimentoSchema = new mongoose.Schema({
+  telefone: { type: String, required: true },
+  valor: { type: Number, required: true },
+  referencia: { type: String, required: true },
+  criadoEm: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Investimento", InvestimentoSchema);
+module.exports = mongoose.model("Investimento", investimentoSchema);
